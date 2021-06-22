7f097b2d9c01e53ba5c8fc7132bdbd2d5c416e27
Samuel Lee <samuel.lee@aquinas.school.nz>
2021-06-22 12:34:48 +1200

    testing db stuff

---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE Elision
+(id int IDENTITY(1,1) PRIMARY KEY,
+percentiile_Rank VARCHAR(40),
+Elision VARCHAR(40),
+Scaled_Score VARCHAR(40),
+Min_Months VARCHAR(40),
+Max_Months VARCHAR(40))
+GO
