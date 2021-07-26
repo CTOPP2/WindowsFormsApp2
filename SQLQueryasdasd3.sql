c69f9f0828c9957c8431fabfffe22bf5f9f26cdc
samalu54 <85590751+samalu54@users.noreply.github.com>
2021-07-27 11:48:50 +1200

    nonword repitition

--- SQLQueryasdasd3.sql
+++ SQLQueryasdasd3.sql
@@ -1,4 +1,4 @@
-﻿bulk insert dbo.phoneme_iso
-from '\\ac-dc2\student$\year13\samuel.cuanang\GitHub\WindowsFormsApp2\Phoneme Isolation Table - Sheet1.csv'
+﻿bulk insert dbo.nonword_repitition
+from '\\ac-dc2\student$\year13\samuel.cuanang\GitHub\WindowsFormsApp2\Nonword Repitition - Sheet1.csv'
 with (FIRSTROW = 2, fieldterminator = ',', rowterminator = '\n')
 go
