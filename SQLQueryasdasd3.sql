a6e9724bb99734dfad6b160fb6521b35a485346d
samalu54 <85590751+samalu54@users.noreply.github.com>
2021-07-27 11:45:38 +1200

    phoneme iso

--- SQLQueryasdasd3.sql
+++ SQLQueryasdasd3.sql
@@ -1,4 +1,4 @@
-﻿bulk insert dbo.memorydigits
-from '\\ac-dc2\student$\year13\samuel.cuanang\GitHub\WindowsFormsApp2\MemoryDigits - Sheet1.csv'
+﻿bulk insert dbo.phoneme_iso
+from '\\ac-dc2\student$\year13\samuel.cuanang\GitHub\WindowsFormsApp2\Phoneme Isolation Table - Sheet1.csv'
 with (FIRSTROW = 2, fieldterminator = ',', rowterminator = '\n')
 go
