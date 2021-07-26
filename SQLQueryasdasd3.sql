094e2f1be0ccc1bbf53b158c17c0d0beaea27eec
samalu54 <85590751+samalu54@users.noreply.github.com>
2021-07-27 11:42:15 +1200

    memory didgits

---
+++ SQLQueryasdasd3.sql
@@ -0,0 +1,4 @@
+﻿bulk insert dbo.memorydigits
+from '\\ac-dc2\student$\year13\samuel.cuanang\GitHub\WindowsFormsApp2\MemoryDigits - Sheet1.csv'
+with (FIRSTROW = 2, fieldterminator = ',', rowterminator = '\n')
+go
---
+++ asdasdasd.sql
@@ -0,0 +1,4 @@
+﻿bulk insert dbo.blendedwords
+from 'H:\GitHub\WindowsFormsApp2\blendedWords - Sheet1.csv'
+with (FIRSTROW = 2, fieldterminator = ',', rowterminator = '\n')
+go
