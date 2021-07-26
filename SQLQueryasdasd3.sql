b79bc4fbba90acd2c1f8cbd7b3bd104d4e61c35f
samalu54 <85590751+samalu54@users.noreply.github.com>
2021-07-27 11:51:17 +1200

    rapid digit naming

--- SQLQueryasdasd3.sql
+++ SQLQueryasdasd3.sql
@@ -1,4 +1,4 @@
-﻿bulk insert dbo.nonword_repitition
-from '\\ac-dc2\student$\year13\samuel.cuanang\GitHub\WindowsFormsApp2\Nonword Repitition - Sheet1.csv'
+﻿bulk insert dbo.rapid_digit_naming
+from '\\ac-dc2\student$\year13\samuel.cuanang\GitHub\WindowsFormsApp2\RapidDigitNaming - Sheet1.csv'
 with (FIRSTROW = 2, fieldterminator = ',', rowterminator = '\n')
 go
