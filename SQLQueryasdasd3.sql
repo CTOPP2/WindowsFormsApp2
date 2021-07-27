ec423a056fa25b82994ca211269e91bf76138097
samalu54 <85590751+samalu54@users.noreply.github.com>
2021-07-27 12:03:20 +1200

    blending non words

--- SQLQueryasdasd3.sql
+++ SQLQueryasdasd3.sql
@@ -1,4 +1,4 @@
-﻿bulk insert dbo.rapid_letter_naming
-from '\\ac-dc2\student$\year13\samuel.cuanang\GitHub\WindowsFormsApp2\Rapid Letter Naming - Sheet1.csv'
+﻿bulk insert dbo.blending_nonwords
+from '\\ac-dc2\student$\year13\samuel.cuanang\GitHub\WindowsFormsApp2\Blending_Nonwords - Sheet1.csv'
 with (FIRSTROW = 2, fieldterminator = ',', rowterminator = '\n')
 go
