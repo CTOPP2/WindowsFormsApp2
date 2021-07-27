4d4ed84dd49c1f68970259dcd5d91dca011a87fa
samalu54 <85590751+samalu54@users.noreply.github.com>
2021-07-27 12:05:33 +1200

    segmenting non words

--- SQLQueryasdasd3.sql
+++ SQLQueryasdasd3.sql
@@ -1,4 +1,4 @@
-﻿bulk insert dbo.blending_nonwords
-from '\\ac-dc2\student$\year13\samuel.cuanang\GitHub\WindowsFormsApp2\Blending_Nonwords - Sheet1.csv'
+﻿bulk insert dbo.segmenting_nonwords
+from '\\ac-dc2\student$\year13\samuel.cuanang\GitHub\WindowsFormsApp2\SegmentingNonwords - Sheet1.csv'
 with (FIRSTROW = 2, fieldterminator = ',', rowterminator = '\n')
 go
